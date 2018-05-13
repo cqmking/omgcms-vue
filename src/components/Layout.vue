dfc814a5337b5883dca1ed349fd2037f02dbb5ba
madfrogyang <madfrogyang@163.com>
2018-05-13 23:53:57 +0800

    update

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -58,10 +58,6 @@
               <template v-for="(item, n) in matchedItems">
                 <el-breadcrumb-item v-if="item.path!='/layout/home'" :to="{ path: item.path }">{{item.meta.label}}</el-breadcrumb-item>
               </template>
-              <!-- <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
-              <el-breadcrumb-item><a href="/">活动管理</a></el-breadcrumb-item>
-              <el-breadcrumb-item>活动列表</el-breadcrumb-item>
-              <el-breadcrumb-item>活动详情</el-breadcrumb-item> -->
             </el-breadcrumb>
 
             <router-view></router-view>
